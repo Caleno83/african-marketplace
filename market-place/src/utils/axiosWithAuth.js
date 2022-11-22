142a7d3d326e73bec8aafbf99a26c1c507cbe8f5
Jorge A Jimenez <57850031+Caleno83@users.noreply.github.com>
2022-11-21 23:20:25 -0500

    Update axiosWithAuth.js

--- market-place/src/utils/axiosWithAuth.js
+++ market-place/src/utils/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://african-american.onrender.com/",
+    baseURL: "https://african-american.up.railway.app/",
     headers: {
       Authorization: token,
     },
